14e78a753b087c011f500bae8b4c92e44f5bbc6e
lsy <yeopy.dev@gmail.com>
2022-09-04 18:31:48 +0900

    first commit

---
+++ src/main/java/com/pabi/pabiback/PabiBackApplication.java
@@ -0,0 +1,13 @@
+package com.pabi.pabiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PabiBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PabiBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/pabi/pabiback/PabiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pabi.pabiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabiBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
